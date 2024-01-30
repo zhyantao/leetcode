7a2d5be9ef9bf65ab3a633441fdfb18c0f15271f
zhyantao <zh6tao@gmail.com>
2024-01-30 23:25:24 +0800

    change project organization

--- cpp/Solution.cpp
+++ cpp/Solution.cpp
@@ -1,41 +1,43 @@
-#include "utils/FileReader.h"
-#include "LeetCode/LC0054.h"
-#include "LeetCode/LC0059.h"
-#include "LeetCode/LC0209.h"
-#include "LeetCode/LC0027.h"
-#include "LeetCode/LC0704.h"
+#include "FileReader.h"
+#include "LC0054.h"
+#include "LC0059.h"
+#include "LC0209.h"
+#include "LC0027.h"
+#include "LC0704.h"
 
 int main(int argc, char *argv[]) {
 	FileReader fileReader;
 	string file_path;
+
+	std::cout << "hello world" << std::endl;
 
 //	{
 //		LC0054 solution;
 //		vector<vector<int>> matrix;
 //
 //		vector<int> res;
-//		file_path = "D:/local_workshop/leetcode/cpp/src/inputs/matrix1.txt";
+//		file_path = "inputs/matrix1.txt";
 //		matrix = fileReader.getMatrix(file_path);
 //		res = solution.spiralOrder(matrix);
 //		for (const auto &t : res)
 //			cout << t << " ";
 //		cout << endl;
 //
-//		file_path = "D:/local_workshop/leetcode/cpp/src/inputs/matrix2.txt";
+//		file_path = "inputs/matrix2.txt";
 //		matrix = fileReader.getMatrix(file_path);
 //		res = solution.spiralOrder(matrix);
 //		for (const auto &t : res)
 //			cout << t << " ";
 //		cout << endl;
 //
-//		file_path = "D:/local_workshop/leetcode/cpp/src/inputs/matrix3.txt";
+//		file_path = "inputs/matrix3.txt";
 //		matrix = fileReader.getMatrix(file_path);
 //		res = solution.spiralOrder(matrix);
 //		for (const auto &t : res)
 //			cout << t << " ";
 //		cout << endl;
 //
-//		file_path = "D:/local_workshop/leetcode/cpp/src/inputs/matrix4.txt";
+//		file_path = "inputs/matrix4.txt";
 //		matrix = fileReader.getMatrix(file_path);
 //		res = solution.spiralOrder(matrix);
 //		for (const auto &t : res)
@@ -56,7 +58,7 @@ int main(int argc, char *argv[]) {
 //
 //	{
 //		LC0209 solution;
-//		file_path = "D:/local_workshop/leetcode/cpp/src/inputs/numbers.txt";
+//		file_path = "inputs/numbers.txt";
 //		vector<int> nums = fileReader.getNumbers(file_path);
 //		int res = solution.minSubArrayLen(697439, nums);
 //		cout << res << endl;
@@ -64,7 +66,7 @@ int main(int argc, char *argv[]) {
 
 //	{
 //		LC0027 solution;
-//		file_path = "D:/local_workshop/leetcode/cpp/src/inputs/numbers.txt";
+//		file_path = "inputs/numbers.txt";
 //		vector<int> nums = fileReader.getNumbers(file_path);
 //		int res = solution.removeElement(nums, 4);
 //		cout << res << endl;
@@ -72,7 +74,7 @@ int main(int argc, char *argv[]) {
 
 	{
 		LC0704 solution;
-		file_path = "D:/local_workshop/leetcode/cpp/src/inputs/numbers.txt";
+		file_path = "inputs/numbers.txt";
 		vector<int> nums = fileReader.getNumbers(file_path);
 		int target = 2;
 		cout << target<< endl;
